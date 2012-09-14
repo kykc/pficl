47db6ee9e1a0b554fbbd0156a2f4c719c462f0a2
kykc <mad.kykc@gmail.com>
2012-09-14 22:37:58 +0300

    ololo2

---
+++ pficl/Fs/FileFilterIterator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace pficl\Fs
+{
+	class FileFilterIterator extends FilterIterator
+	{
+		protected $customFilter = NULL;
+
+		public function accept()
+		{
+			$pathName = $this->getInnerIterator()->current()->getPathName();
+
+			if (is_readable($pathName) && is_file($pathName) && $this->applyCustomFilter())
+			{
+				return TRUE;
+			}
+			else
+			{
+				return FALSE;
+			}
+		}
+
+		public function __construct(Iterator $i, $customFilter)
+		{
+			parent::__construct($i);
+			$this->customFilter = $customFilter;
+		}
+
+		protected function applyCustomFilter()
+		{
+			if ($this->customFilter)
+			{
+				$filter = $this->customFilter;
+				return $filter($this->getInnerIterator()->current());
+			}
+			else
+			{
+				return TRUE;
+			}
+		}
+	}
+}
+?>
