10dda34a1aaa2206ea665bdf55eded7edffe9e10
kykc <mad.kykc@gmail.com>
2013-03-15 17:44:56 +0200

    Extended custom PDO with some functions

---
+++ pficl/Db/Exception.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace pficl\Db
+{
+	class Exception extends \Exception
+	{
+	}
+
+	class MultipleRowsException extends Exception
+	{
+	}
+
+	class InvalidStorageNameException extends Exception
+	{
+	}
+
+	class InvalidFieldListException extends Exception
+	{
+	}
+}
+
+?>
--- pficl/Db/PDO.php
+++ pficl/Db/PDO.php
@@ -6,6 +6,68 @@ namespace pficl\Db
 
 	final class PDO extends \PDO
 	{
+		public function queryFetchAllAssoc($sql)
+		{
+			return $this->query($sql)->fetchAll(self::FETCH_ASSOC);
+		}
+
+		public function fetchRow($storageName, array $fieldList)
+		{
+			$this->checkStorageName($storageName)->checkFieldList($fieldList);
+
+			$sql = 'SELECT * FROM `'.$storageName.'` WHERE 1 ';
+
+			foreach ($fieldList as $fieldName => $fieldValue)
+			{
+				$sql .= 'AND `'.$fieldName.'` = :'.$fieldName.' ';
+			}
+
+			$st = $this->prepare($sql);
+
+			$st->execute($fieldList);
+
+			if ($st->rowCount() === 0)
+			{
+				return NULL;
+			}
+			elseif ($st->rowCount() === 1)
+			{
+				return $st->fetch(self::FETCH_ASSOC);
+			}
+			else
+			{
+				throw new MultipleRowsException();
+			}
+		}
+
+		public function checkStorageName($storageName)
+		{
+			if (!(is_string($storageName) && strval($storageName) && preg_match('/^[\w_\d]{1,}$/', $storageName)))
+			{
+				throw new InvalidStorageNameException();
+			}
+
+			return $this;
+		}
+
+		public function checkFieldList(array $fieldList)
+		{
+			foreach ($fieldList as $fieldName => $fieldValue)
+			{
+				if (!(is_string($fieldName) && strval($fieldName) && preg_match('/^[\d_\w]{1,}$/', $fieldName)))
+				{
+					throw new InvalidFieldListException();
+				}
+
+				if (!(is_int($fieldValue) || is_string($fieldValue) || is_float($fieldValue) || is_null($fieldValue) || is_bool($fieldValue)))
+				{
+					throw new InvalidFieldListException();
+				}
+			}
+
+			return $this;
+		}
+
 		public function mkIn(array $values)
 		{
 			$obj = $this;
