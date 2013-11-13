642d750762afe93d3e527eb8921826387a93758e
kykc <mad.kykc@gmail.com>
2013-11-13 11:15:34 +0200

    Auth

---
+++ pficl/Auth/Util.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace pficl\Auth
+{
+	abstract class Util
+	{
+		public static function getToken()
+		{
+			if (array_key_exists('HTTP_TOMATL_AUTH', $_SERVER) && \pficl\Web\Util::isLocalIp($_SERVER['REMOTE_ADDR']))
+			{
+				return $_SERVER['HTTP_TOMATL_AUTH'];
+			}
+			else
+			{
+				return NULL;
+			}
+		}
+	}
+}
+
+?>
