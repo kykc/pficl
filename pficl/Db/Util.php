654a07c726554101f3ee784c30484e4673f9b37f
kykc <mad.kykc@gmail.com>
2014-01-24 15:08:28 +0200

    Mysql client xml format parser

--- pficl/Db/Util.php
+++ pficl/Db/Util.php
@@ -8,6 +8,30 @@ namespace pficl\Db
 		{
 			return rtrim(str_repeat('?, ', count($values)), ', ');
 		}
+
+		final public static function fromMysqlClientXml($xml)
+		{
+			$doc = new DOMDocument('1.0');
+			$doc->loadXML($xml);
+
+			$rows = $doc->getElementsByTagName('row');
+
+			$result = array();
+
+			foreach ($rows as $row)
+			{
+				$rrow = array();
+
+				foreach ($row->getElementsByTagName('field') as $field)
+				{
+					$rrow[$field->getAttribute('name')] = $field->nodeValue;
+				}
+
+				$result[] = $rrow;
+			}
+
+			return $result;
+		}
 	}
 }
 
